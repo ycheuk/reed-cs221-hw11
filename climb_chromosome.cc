eff113af206c0a757e4fe150a3c848472ac93a8a
AnastasiyaV <AnastasiyaV@c133h054.wless.reed.edu>
2022-04-27 16:35:18 -0700

    Started mutate()

--- climb_chromosome.cc
+++ climb_chromosome.cc
@@ -2,7 +2,12 @@
 
 #include "climb_chromosome.hh"
 
-void
-ClimbChromosome::mutate()
-{
+void ClimbChromosome::mutate(){
+  std::uniform_int_distribution<int> dist(0, order_.size()-1);
+  auto fitness = get_fitness();
+
+  long unsigned int parent1 = dist(generator_);
+  long unsigned int parent2 = parent1 - 1;
+
+  std::swap(order_[parent1], order_[parent2]);
 }
