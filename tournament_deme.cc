24ee48f6068cf41ee99e1d7c9ce523fd14baeefd
vaughnzaayer <95721454+vaughnzaayer@users.noreply.github.com>
2022-05-03 08:40:31 -0700

    Add files via upload

--- tournament_deme.cc
+++ tournament_deme.cc
@@ -2,45 +2,50 @@
 #include "climb_chromosome.hh"
 #include "cities.hh"
 
+#include <cassert>
 
 
 
 Chromosome* TournamentDeme::select_parent()
 {
 	// Set the number of slots in the tournament
-	int slots = std::pow(2, 5);	// 2 ^ 5
+	int slots = std::pow(2, 3);	
 
 	std::vector<Chromosome*> roster;
 	roster.reserve(slots);
 
 	// Fill all available slots in roster
-	std::uniform_int_distribution<int> range(0, pop_.size());
-	for(int i; i < slots; i++) {
+	std::uniform_int_distribution<int> range(0, pop_.size() - 1);
+	for(int i = 0; i < slots; i++) {
 		roster.push_back(pop_.at(range(generator_)));
 	}
 
 	// Tournament loop
 	std::vector<Chromosome*> nextRound;
+	nextRound.reserve(roster.size());
 	while(slots > 1) {
 
+		std::cout << slots << std::endl;
+
 		slots = slots / 2;
-		nextRound.reserve(slots);
 		// For every 2 chromosomes, pit them against each other 
 		// Victor moves on to the next round
-		for(long unsigned int i; i < roster.size(); i += 2) {
+		std::cout << roster.size() << std::endl;
+		for(long unsigned int i = 0; i < roster.size(); i += 2) {
 			auto chromosome1 = roster[i];
 			auto chromosome2 = roster[i + 1];
 			if(chromosome1 -> get_fitness() >= chromosome2 -> get_fitness()) {
 				nextRound.push_back(chromosome1);
+				std::cout << i << " - " << chromosome1 -> get_fitness() << std::endl;
 			}else {
 				nextRound.push_back(chromosome2);
+				std::cout << i + 1 << " - " << chromosome2 -> get_fitness() << std::endl;
+
 			}
 		}
 
 		roster.swap(nextRound);
-		
 		nextRound.clear();
-
 
 	}
 
